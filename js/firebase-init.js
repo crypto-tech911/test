a314a387b5e10c821c45ebba7adf116bb8b95a9d
crypto-tech911 <mr8436883@gmail.com>
2026-09-24 00:59:32 +0600

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,13 @@
+// Initializes the Firebase app and exports the shared SDK instances.
+// Every other JS file in this project should import auth/db from HERE,
+// not re-initialize Firebase itself.
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
